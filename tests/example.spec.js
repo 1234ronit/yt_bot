49a0d72bf48d0798d260d48109d155ee5565db42
Ronit Roy <36254743+1234ronit@users.noreply.github.com>
2022-07-25 07:44:28 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,22 +10,22 @@ const n = 10;
 for (let i = 0; i < n; i++) {
   test(`Youtube1- ${i} `, async ({ page }) => {
     await page.goto(url1);
+    console.log(url1);
     await sleep(600*1000);
     for (let i = 0; i < 10; i++) {
       await page.click('//video[@class="video-stream html5-main-video"]');
       await sleep(1000);
     }
-  console.log(url);
   });
 };
 for (let i = 0; i < n; i++) {
   test(`Youtube2- ${i} `, async ({ page }) => {
     await page.goto(url2);
+    console.log(url2);
     await sleep(600*1000);
     for (let i = 0; i < 10; i++) {
       await page.click('//video[@class="video-stream html5-main-video"]');
       await sleep(1000);
     }
-    console.log(url);
   });
 };
