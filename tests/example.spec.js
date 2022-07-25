2409f9856ee76238f37379c571bf7ecd1a33ee06
Ronit Roy <36254743+1234ronit@users.noreply.github.com>
2022-07-25 07:09:07 +0530

    Add files via upload

---
+++ tests/example.spec.js
@@ -0,0 +1,29 @@
+const { test } = require('@playwright/test');
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+const url1 = 'https://youtu.be/PtWg-f52VfU'
+const url2 = 'https://www.youtube.com/watch?v=pI6G4F_tze0&t=1285s'
+
+const n = 10;
+
+for (let i = 0; i < n; i++) {
+  test(`Youtube1- ${i} `, async ({ page }) => {
+    await page.goto(url1);
+    await sleep(600*1000);
+    for (let i = 0; i < 10; i++) {
+      await page.click('//video[@class="video-stream html5-main-video"]');
+      await sleep(1000);
+    }
+  });
+};
+for (let i = 0; i < n; i++) {
+  test(`Youtube2- ${i} `, async ({ page }) => {
+    await page.goto(url2);
+    await sleep(600*1000);
+    for (let i = 0; i < 10; i++) {
+      await page.click('//video[@class="video-stream html5-main-video"]');
+      await sleep(1000);
+    }
+  });
+};
