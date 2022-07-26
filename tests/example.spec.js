812c996bab4f796b5850a8863de72825b6fd74ab
Ronit Roy <36254743+1234ronit@users.noreply.github.com>
2022-07-26 17:49:40 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,20 +2,22 @@ const { test } = require('@playwright/test');
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
-const url1 = 'https://youtu.be/PtWg-f52VfU'
+const url1 = 'https://youtu.be/rqBHt2E4Cls'
 const url2 = 'https://www.youtube.com/watch?v=pI6G4F_tze0&t=1285s'
 
 const n = 10;
 for (let i = 0; i < n; i++) {
-  test(`Youtube1- ${i} `, async ({ page }) => {
+  test(`Youtube1- ${i} `, async ({ context }) => {
     await sleep(5*1000);
     for (let j = 0; j < 100; j++) {
+      let page = await context.newPage();
       await page.goto(url1);
       console.log(`Youtube1- ${i} `, url1);
 
       await page.click('button.ytp-button.ytp-settings-button');
       await page.click("//div[contains(text(),'Quality')]");
       await page.click("//span[contains(string(),'144p')]");
+      console.log("quality changed");
 
       await sleep(300*1000);
       await page.close();
