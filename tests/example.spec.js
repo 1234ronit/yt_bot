89393e0ea408af6f7a7dda760e335d32724ceeae
Ronit Roy <36254743+1234ronit@users.noreply.github.com>
2022-07-27 02:33:40 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ const sleep = (milliseconds) => {
 const url1 = 'https://youtu.be/rqBHt2E4Cls'
 const url2 = 'https://www.youtube.com/watch?v=pI6G4F_tze0&t=1285s'
 
-const n = 2;
+const n = 3;
 for (let i = 0; i < n; i++) {
   test(`Youtube1- ${i} `, async ({ context }) => {
     await sleep(10*1000);
