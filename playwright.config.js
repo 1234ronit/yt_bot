2fd16d80a4adbdf9686ef52c5efa141ba8e0af90
Ronit Roy <36254743+1234ronit@users.noreply.github.com>
2022-07-26 17:10:57 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ const { devices } = require('@playwright/test');
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 36 * 100 * 1000,
+  timeout: 360 * 100 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -36,7 +36,7 @@ const config = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 36 * 100 * 1000,
+    actionTimeout: 600 * 1000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
